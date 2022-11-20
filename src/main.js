12227fe42baac8c8072917de51a450ac3cabf81d
Dmytro Skoroplias <87.dimuch@gmail.com>
2022-11-20 16:47:16 +0200

    start1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+const formRef = document.querySelector(".form");
+
+formRef.addEventListener("submit", formData);
+
+function formData(event) {
+  event.preventDefault();
+  const value = event.target.message.value.trim();
+  if (!value) return;
+
+  const data = createDataObj(value);
+
+  event.target.reset();
+}
+
+function createDataObj(value) {
+  return { value: value, id: Date.now(), checked: false };
+}
